da1b994b222ceee7c2405729c3787e4083ce3ced
Vivian Chen <yc5519@nyu.edu>
2026-04-18 11:44:13 +0200

    add readme

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -2,6 +2,8 @@ const codes = require("../utils/responseCodes");
 
 function requireAuth(req, res, next) {
     if (!req.session || !req.session.userId) {
+        console.log(req.session);
+        console.log(req.session.userId);
         return res.status(codes.UNAUTHORIZED).json({error: "Authentication required"});
     }
     next();
